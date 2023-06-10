a88d73571f61fb15f66318b483bc809c5c2ca211
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-06-10 15:42:38 +0800

    Adds styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import './style.css';
-import { domElements } from "./addContent";
-import bgImage from "./assets/bg.jpg";
 
-import { adjustMainSize } from './sizeAdjust';
+import { sizeAdjust } from './apps/sizeAdjust';
+import { changeContent } from './apps/changeContent';
+import { addDomStructure } from './apps/addDomStructure';
+import { addHeaderContent } from './apps/addHeaderContent';
+import { addMainContent } from './apps/addMainContent';
+import { addFooterContent } from './apps/addFooterContent';
 
-// domElements();
+// Append basic DOM elements;
+addDomStructure.appendBodyElements();
 
+// Append header contents
+addHeaderContent.appendHeaderContents()
 
 // Adjust the padding-top of main on resize
 // Note: header position is fixed
-window.addEventListener('load', adjustMainSize);
-window.addEventListener('resize', adjustMainSize);
+window.addEventListener('load', sizeAdjust.adjustMainSize);
+window.addEventListener('resize', sizeAdjust.adjustMainSize);
+
+// Appends main container and append home contents on load
+addMainContent.appendMainContainer();
+changeContent.onLoadPage(); //home contents are loaded
+
+// Append footer contents
+addFooterContent.appendFooterContent();
+
+
+// Add Event Listeners to tab changing buttons
+const tabBtns = document.querySelectorAll('div#tab-btns button');
+tabBtns.forEach(btn => btn.addEventListener('click', changeContent.changeTab));
+
+
