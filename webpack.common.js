71384312b74df00db4fb10b9cc4e76d110b968bd
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-06-17 19:16:54 +0800

    Remove .ico from assets test

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
                 use: ["html-loader"]
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource'
             },
 
